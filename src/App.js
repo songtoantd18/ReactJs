169b3541fd3722653ef4460b56c9e74f4cda1c16
songtoantd18 <songtoantd18@gmail.com>
2022-09-23 20:47:22 +0700

    vd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import Medal from './Medal';
+// import Medal from './Medal';
+import ListTodos from './Render';
 
 
 
@@ -8,52 +9,46 @@ import Medal from './Medal';
 function App() {
 
   return (
-    <div className='layout'>
-      <Medal
-      boder= {'yellow'}
-      title = {'nhom1'}
-      groupAvartar={'dua xe'}
-      numberMember= {'dasdasdsa'}
-      project={'app thoi tiet'}
-      text={'dasdasdasdas'}
-      logoTask={'dasdasdasdas'}
-      colorIndex={'blue'}></Medal>
-        <Medal
-      boder= {'yellow'}
-      title = {'nhom1'}
-      groupAvartar={'dua xe'}
-      numberMember= {'dasdasdsa'}
-      project={'app thoi tiet'}
-      text={'dasdasdasdas'}
-      logoTask={'dasdasdasdas'}
-      colorIndex={'red'}>
+    <div>
+      <ListTodos/>
 
-      </Medal>
-      <Medal
-      boder= {'yellow'}
-      title = {'nhom1'}
-      groupAvartar={'dua xe'}
-      numberMember= {'dasdasdsa'}
-      project={'app thoi tiet'}
-      text={'dasdasdasdas'}
-      logoTask={'dasdasdasdas'}
-      colorIndex={'#00ff00'}>
+    </div>
+ 
+    // <div className='layout'>
+    //   <Medal
+    //   boder= {'yellow'}
+    //   title = {'nhom1'}
+    //   groupAvartar={'dua xe'}
+    //   numberMember= {'dasdasdsa'}
+    //   project={'app thoi tiet'}
+    //   text={'dasdasdasdas'}
+    //   logoTask={'dasdasdasdas'}
+    //   colorIndex={'blue'}></Medal>
+    //     <Medal
+    //   boder= {'yellow'}
+    //   title = {'nhom1'}
+    //   groupAvartar={'dua xe'}
+    //   numberMember= {'dasdasdsa'}
+    //   project={'app thoi tiet'}
+    //   text={'dasdasdasdas'}
+    //   logoTask={'dasdasdasdas'}
+    //   colorIndex={'red'}>
+
+    //   </Medal>
+    //   <Medal
+    //   boder= {'yellow'}
+    //   title = {'nhom1'}
+    //   groupAvartar={'dua xe'}
+    //   numberMember= {'dasdasdsa'}
+    //   project={'app thoi tiet'}
+    //   text={'dasdasdasdas'}
+    //   logoTask={'dasdasdasdas'}
+    //   colorIndex={'#00ff00'}>
         
-      </Medal>
+    //   </Medal>
       
      
-    </div>
-
-  //   <div>
-  //   <p>title:task1</p>
-  //   <p>creator : loc</p>
-  //   <p>status :new</p>
-  //   <hr/>
-  //   <p>description this is a task</p>
-    
-  // </div>
-
-    
+    // </div>
   );
 }
 
---
+++ src/Render.jsx
@@ -0,0 +1,102 @@
+ import TodoItem from "./todoItem";
+ 
+ const todos = [ 
+   { id: 1,
+
+     title: 'Task 1', 
+
+     creator: 'Loc', 
+
+    
+   }, 
+
+   { 
+      id: 2,
+
+     title: 'Task 2', 
+
+     creator: 'Loc', 
+
+    
+
+   }, 
+
+   { 
+      id: 3,
+
+     title: 'Task 3', 
+
+     creator: 'Loc', 
+
+     
+   }, 
+
+   { 
+      id: 4,
+
+     title: 'Task 4', 
+
+     creator: 'Loc', 
+
+    
+   }, 
+
+   { 
+      id: 5,
+
+     title: 'Task 5', 
+
+     creator: 'Loc', 
+
+     
+   }, 
+
+   { 
+      id: 6,
+     title: 'Task 6', 
+
+     creator: 'Loc', 
+
+ 
+
+   }, 
+
+   { 
+      id: 7,
+
+     title: 'Task 7', 
+
+     creator: 'Loc', 
+
+
+   }, 
+
+   { 
+      id: 8,
+
+     title: 'Task 8', 
+
+     creator: 'Loc', 
+
+    
+
+   }, 
+
+ ]; 
+ 
+ 
+ 
+ const ListTodos =()=>{
+    return todos.map(item =>
+      <TodoItem
+      key={item.id}
+      title={item.title}
+      status={item.status}
+      />
+
+      
+      )
+
+ }
+
+ export default ListTodos

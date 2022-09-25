6e313a1a46ca1328d6954f769700e6987dcfa448
songtoantd18 <songtoantd18@gmail.com>
2022-09-25 11:36:57 +0700

    vidu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-// import Medal from './Medal';
+import Medal from './Medal';
 import ListTodos from './Render';
 
 
@@ -9,46 +9,46 @@ import ListTodos from './Render';
 function App() {
 
   return (
-    <div>
-      <ListTodos/>
+    // <div>
+    //   <ListTodos/>
 
-    </div>
+    // </div>
  
-    // <div className='layout'>
-    //   <Medal
-    //   boder= {'yellow'}
-    //   title = {'nhom1'}
-    //   groupAvartar={'dua xe'}
-    //   numberMember= {'dasdasdsa'}
-    //   project={'app thoi tiet'}
-    //   text={'dasdasdasdas'}
-    //   logoTask={'dasdasdasdas'}
-    //   colorIndex={'blue'}></Medal>
-    //     <Medal
-    //   boder= {'yellow'}
-    //   title = {'nhom1'}
-    //   groupAvartar={'dua xe'}
-    //   numberMember= {'dasdasdsa'}
-    //   project={'app thoi tiet'}
-    //   text={'dasdasdasdas'}
-    //   logoTask={'dasdasdasdas'}
-    //   colorIndex={'red'}>
+    <div className='layout'>
+      <Medal
+      boder= {'yellow'}
+      title = {'nhom1'}
+      groupAvartar={'dua xe'}
+      numberMember= {'dasdasdsa'}
+      project={'app thoi tiet'}
+      text={'dasdasdasdas'}
+      logoTask={'dasdasdasdas'}
+      colorIndex={'blue'}></Medal>
+        <Medal
+      boder= {'yellow'}
+      title = {'nhom1'}
+      groupAvartar={'dua xe'}
+      numberMember= {'dasdasdsa'}
+      project={'app thoi tiet'}
+      text={'dasdasdasdas'}
+      logoTask={'dasdasdasdas'}
+      colorIndex={'red'}>
 
-    //   </Medal>
-    //   <Medal
-    //   boder= {'yellow'}
-    //   title = {'nhom1'}
-    //   groupAvartar={'dua xe'}
-    //   numberMember= {'dasdasdsa'}
-    //   project={'app thoi tiet'}
-    //   text={'dasdasdasdas'}
-    //   logoTask={'dasdasdasdas'}
-    //   colorIndex={'#00ff00'}>
+      </Medal>
+      <Medal
+      boder= {'yellow'}
+      title = {'nhom1'}
+      groupAvartar={'dua xe'}
+      numberMember= {'dasdasdsa'}
+      project={'app thoi tiet'}
+      text={'dasdasdasdas'}
+      logoTask={'dasdasdasdas'}
+      colorIndex={'#00ff00'}>
         
-    //   </Medal>
+      </Medal>
       
      
-    // </div>
+    </div>
   );
 }
 

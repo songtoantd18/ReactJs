570ab8eef032389c8a370efe69b11937079636f4
songtoantd18 <songtoantd18@gmail.com>
2022-10-08 23:09:39 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main"
-import Taskbar from "./components/Taskbar"
-
 
 function App() {
-  return (
-    <div className="total">
-      <div className="container">khong co gi 
-        <Header></Header>
-        <div className="taskbar_main">
-          <Taskbar></Taskbar>
-          <Main></Main>
-
-
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
